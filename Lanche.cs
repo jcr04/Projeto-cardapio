d85de50bcbba10efbc499531f5429219b4f524bb
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2023-03-09 15:17:28 -0300

    Create Lanches

---
+++ Lanche.cs
@@ -0,0 +1,9 @@
+public class Lanche
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string ImagemUrl { get; set; }
+}
+
